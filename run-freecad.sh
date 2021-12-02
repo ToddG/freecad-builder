8898baa019f81a8e5e668ac6a4733d525d6de5af
Todd Greenwood-Geer <toddg@envirosoftwaresolutions.com>
2021-12-01 22:34:05 -0800

    add run-freecad.sh

---
+++ run-freecad.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+xhost +
+
+CURDIR=`(pwd)`
+fc_source=$CURDIR/freecad-source
+fc_build=$CURDIR/freecad-bin
+fc_other=$CURDIR/freecad-other
+
+docker run -it --rm \
+    -v $fc_source:/mnt/source \
+    -v $fc_build:/mnt/build \
+    -v $fc_other:/mnt/other \
+    -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+    freecad-container:latest \
+    /mnt/build/bin/FreeCAD
+
